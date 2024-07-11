1c705971f0e87aa0f143e62aff0f0523cf8b6b7d
gdmipro <gdmipro@gmail.com>
2024-07-11 20:22:30 +0300

    commit2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,6 +11,7 @@ public class App {
         System.out.println("TESTNEWBRANCH");
 
         System.out.println("Commit 1");
+        System.out.println("commit2");
     }
 
     public static void main(String[] args) {
